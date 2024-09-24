b6eed123ab9a1785c890b38d9caaeb9a4aec3c1a
hwanginseong@TIP219 <3dlstjd3@gmail.com>
2024-09-24 10:02:05 +0900

    캐릭터 출력

--- character_grass.py
+++ character_grass.py
@@ -1,21 +1,28 @@
 from pico2d import *
 
+open_canvas()
+
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+
 def run_rectangle():
     print('rectangle')
     pass
 
 def run_circle():
     print('circle')
+
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(1)
     pass
 
-
-open_canvas()
-
-grass = load_image('grass.png')
-boy = load_image('character.png')
 
 while True:
     run_rectangle()
     run_circle()
+    break
 
+    
 close_canvas()
